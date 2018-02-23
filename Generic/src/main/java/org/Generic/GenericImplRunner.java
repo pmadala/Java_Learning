a6ef1881c12300ffd599c73a8a0823b8b8fcb905
Priyambada Madala <35917175+pmadala@users.noreply.github.com>
2018-02-23 12:22:41 +0530

    Fixes #16

--- Generic/src/main/java/org/Generic/GenericImplRunner.java
+++ Generic/src/main/java/org/Generic/GenericImplRunner.java
@@ -8,22 +8,22 @@ import org.Generic.model.TxtFile;
  */
 public class GenericImplRunner {
 	public static void main(String[] args) throws Exception {
-		Object sortType = GenericUtils.getEnumValueByType("SortType", 1);
+		Object sortType = GenericUtils.getEnumValueByType(Class.forName(SortType.class.getName()), 1);
 		if (sortType == SortType.ASC) {
 			System.out.println("Enum object found");
 		}
 		
-		Object fileType = GenericUtils.getEnumValueByType("FileTypes", new TxtFile()).toString();
+		Object fileType = GenericUtils.getEnumValueByType(Class.forName(FileTypes.class.getName()), new TxtFile()).toString();
 		if (FileTypes.txt.toString().equals(fileType)) {
 			System.out.println("Enum object found");
 		}
 		
-		Object stringType = GenericUtils.getEnumValueByType("StringEnum", "five").toString();
+		Object stringType = GenericUtils.getEnumValueByType(Class.forName(StringEnum.class.getName()), "five").toString();
 		if (StringEnum.FIVE.getValue().equals(stringType)) {
 			System.out.println("Enum object found");
 		}
 		
-		Object dataType = GenericUtils.getEnumValueByType("DataTypes", 1.0f).toString();
+		Object dataType = GenericUtils.getEnumValueByType(Class.forName(DataTypes.class.getName()), 1.0f).toString();
 		if (DataTypes.FLOATVALUE.toString().equals(dataType)) {
 			System.out.println("Enum object found");
 		}
--- Generic/src/main/java/org/Generic/GenericUtils.java
+++ Generic/src/main/java/org/Generic/GenericUtils.java
@@ -11,17 +11,16 @@ public class GenericUtils {
 	
 	/**
 	 * Generic method for getting the enum object by providing the following inputs
-	 * All the enum classes needs to be in org.Generic package .
+	 * 
 	 * Further package name can be generalized 
 	 * 
-	 * @param enumName Name of the enum class
+	 * @param class1 Name of the enum class
 	 * @param value value of the enum object 
 	 * @return
 	 * @throws Exception
 	 */
-	public static <T,R> Object getEnumValueByType(String enumName, T value) throws Exception {
-		Class<?> c = Class.forName("org.Generic."+enumName);
-		Object[] objects = c.getEnumConstants();
+	public static <T,R> Object getEnumValueByType(Class<?> class1, T value) throws Exception {
+		Object[] objects = class1.getEnumConstants();
 		Object returnVal = null;
 		for(Object obj : objects) {
 			if (value.toString().equals(obj.toString())) {
